166d934786eb14c9e2a4314a5b2a6a21b7d4600c
Christopher Forti <christopher.forti.79@gmail.com>
2025-02-14 19:44:25 -0500

    Commented out code in server.ts, for debugging purposes.

--- src/server.ts
+++ src/server.ts
@@ -4,18 +4,18 @@ import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use("/users", userRouter);
+// app.use("/users", userRouter);
 
 app.get("/hello", (req, res) => {
   console.log(req.user, "This is the user");
   res.json({ serverMessage: "Hello world!" });
 });
 
-import { rootRouter } from "./routes/root.js";
-import { userRouter } from "./routes/user.js";
+// import { rootRouter } from "./routes/root.js";
+// import { userRouter } from "./routes/user.js";
 
-app.use("/", rootRouter);
-app.use("/users", userRouter);
+// app.use("/", rootRouter);
+// app.use("/users", userRouter);
 
 app.listen(PORT, () => {
   if (process.env.NODE_ENV === "development") {
