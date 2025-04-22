d4a0417b55f0eb3deceaaec2af50daca0657aeda
Christopher Forti <christopher.forti.79@gmail.com>
2025-04-21 20:34:06 -0400

    Added wav file to project, and worked wav router in server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import userRouter from "./routes/user-route.js";
 import { rootRouter } from "./root.js";
 import { authenticate } from "./middleware/auth.js";
+import { wavRouter } from "./routes/wav-route.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -13,6 +14,7 @@ app.use(authenticate);
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
+app.use("/wavs", wavRouter);
 
 app.get("/hello", (req, res) => {
   console.log(req.user, "This is the user");
